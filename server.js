a9315c0e40a1cfd08ac49f3e867106d96cfe39e3
kateemillerhd <kateemillerhd@gmail.com>
2025-07-05 02:45:43 +0000

    Created server.js for main express setup

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// main functions - enter point
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Image Search Abstraction Layer is running.');
+});
+
+mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+.catch((err) => {
+  console.error('MonogDB connection error:', err);
+});
