44ca2acf52c0d121532fc68f16ad1adbe5cec7cf
kateemillerhd <kateemillerhd@gmail.com>
2025-07-05 15:03:52 +0000

    remnoving frontend for project

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+  res.send('Image Search Abstraction Layer is running');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
