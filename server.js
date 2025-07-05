f0408b39e0a51f9df85fa2081a12b204cc87d46c
kateemillerhd <kateemillerhd@gmail.com>
2025-07-05 14:23:10 +0000

    created UI

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const apiRoutes = require('./routes/api');
 
@@ -8,14 +9,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/', (req, res) => {
   res.send('Image Search Abstraction Layer is running.');
 });
 
-mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
