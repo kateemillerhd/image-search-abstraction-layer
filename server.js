8542cbddc59fed4274c7c39b5d6efab89c6a0247
kateemillerhd <kateemillerhd@gmail.com>
2025-07-05 14:29:08 +0000

    delete unneccessary line

--- server.js
+++ server.js
@@ -17,10 +17,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/', (req, res) => {
-  res.send('Image Search Abstraction Layer is running.');
-});
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
